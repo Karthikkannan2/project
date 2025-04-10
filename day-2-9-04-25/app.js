7dffc83c14edb97b420dc0e33b738526330559ac
Karthikkannan2 <karthik8861084325@gmail.com>
2025-04-10 11:36:14 +0530

    Modified HTML,CSS and JavaScript

---
+++ day-2-9-04-25/app.js
@@ -0,0 +1,58 @@
+// Retrive todo from local storage or initialize or initialize an empty array
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList =  document.getElementById("todoList");
+const todoCount =  document.getElementById("todoCount");
+const addButton =  document.querySelector(".btn");
+const deleteButton =  document.getElementById("deleteButton");
+
+// initialize
+document.addEventListener("DOMContentLoaded", function (){
+    addButton.addEventListener("click",addTask);
+    todoInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click",deleteAllTask);
+    displayTask()
+});
+
+function addTask(){
+const newTask = todoInput.value.trim();
+if(newTask !== ""){
+ todo.push({
+    text: newTask, 
+    disabled: false,
+ }); 
+ saveToLocalStorage();
+ todoInput.value = "";
+ displayTask() 
+}
+}
+
+function deleteAllTask(){
+
+}
+
+function displayTask(){
+todoList.innerHTML = "";
+todo.forEach((item, index) => {
+    const p = document.createElement("p");
+    p.innerHTML = `
+        <div class="todo-container">
+            <input type="checkbox" class="todo-checkbox" id="input-${index}" ${item.disabled ? "checked" : ""}>
+            <p id="todo-${index}" class="${item.disabled ? "disabled" : ""}" onclick="editTask(${index})">${item.text}</p>
+        </div>
+    `;
+    p.querySelector(".todo-checkbox").addEventListener("change", () =>{
+        toggleTask(index);
+    });
+    todoList.append(p);
+});
+}
+
+function saveToLocalStorage(){
+    localStorage.setItem("todo",JSON.stringify(todo));
+}
